d0fd7717f89df4595e0f33a852fc06a18eea01ad
Pavel Gabriel <alovak@gmail.com>
2020-06-19 19:01:23 +0300

    point logo to https://vaulty.co

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,8 +14,10 @@ module.exports = {
       logo: {
         alt: 'Vaulty',
         src: 'img/vaulty-logo.svg',
+      href: 'https://vaulty.co/',
       },
       links: [
+        { label: 'Documentation', to: '/', position: 'right', activeBaseRegex: '/(intro|quick|reference)' },
         { label: 'Cookbooks', to: '/cookbooks/intro', position: 'right', activeBasePath: '/cookbooks' },
         { label: 'GitHub', href: 'https://github.com/vaulty-co/vaulty', position: 'right' },
       ],
