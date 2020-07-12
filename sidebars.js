1acfd8bb25c7d586b6be9fc77fd79e78bea78e96
Pavel Gabriel <alovak@gmail.com>
2020-07-12 21:40:00 +0300

    add encryption backends (#5)

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   docs: {
     'Getting Started': ['intro', 'quick'],
-    Reference: ['reference/configuration', 'reference/routing', 'reference/transformations', 'reference/actions', 'reference/reverse-proxy', 'reference/forward-proxy'],
+    Reference: ['reference/configuration', 'reference/routing', 'reference/transformations', 'reference/actions', 'reference/encryption-backends', 'reference/reverse-proxy', 'reference/forward-proxy'],
     'Cookbooks': ['cookbooks/intro', 'cookbooks/email-list-with-protected-emails', 'cookbooks/safe-sensitive-data-import'],
   },
 };
